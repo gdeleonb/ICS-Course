8b12fc3e6309d458ef72bdda9e4bdd0e6acf62ad
nazgus <gus@deleon.org.mx>
2017-05-28 12:58:56 -0700

    Split comments into multiple lines

--- ch02/ageInSeconds.rb
+++ ch02/ageInSeconds.rb
@@ -1,3 +1,6 @@
-# This one is simple enough, just do seconds times minutes times hours times days, where days is days in a year times number of years, plus leap days plus days since my birthday. If you really really wanted to, you could count from the time of day you were born, but to be honest that seems a little excessive.
+# This one is simple enough, just do seconds times minutes times hours times
+# days, where days is days in a year times number of years, plus leap days plus
+# days since my birthday. If you really really wanted to, you could count from
+# the time of day you were born, but to be honest that seems a little excessive.
 
 puts 'I am ' + (60 * 60 * 24 * (365 * 20 + 5 + 88)).to_s + ' seconds old.'
--- ch02/minutesInDecade.rb
+++ ch02/minutesInDecade.rb
@@ -1,3 +1,5 @@
-# Depending on the decade, there's either two or three leap years in it. So we have minutes times hours times days, where days has a +2 or +3 for each option.
+# Depending on the decade, there's either two or three leap years in it. So we
+# have minutes times hours times days, where days has a +2 or +3 for each
+# option.
 
 puts 'There are ' + (60 * 24 * (365 * 10 + 2)).to_s + ' or ' + (60 * 24 * (365 * 10 + 3)).to_s + ' minutes in a decade, depending on the number of leap years.'
