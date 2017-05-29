554a337fdc1d50a99a045471220eccae1bf449df
nazgus <gus@deleon.org.mx>
2017-05-29 15:19:26 -0700

    Split up response into three parts

--- ch02/minutesInDecade.rb
+++ ch02/minutesInDecade.rb
@@ -2,4 +2,7 @@
 # have minutes times hours times days, where days has a +2 or +3 for each
 # option.
 
-puts 'There are ' + (60 * 24 * (365 * 10 + 2)).to_s + ' or ' + (60 * 24 * (365 * 10 + 3)).to_s + ' minutes in a decade, depending on the number of leap years.'
+puts 'The number of minutes in a decade depends on the number of leap years in said decade.'
+puts 'If there are two leap years, then we have ' + ((365 * 10 + 2) * 60 * 24).to_s + ' minutes.'
+puts 'If there are three leap years, then we have ' + ((365 * 10 + 3) * 60 * 24).to_s + ' minutes.'
+puts 'For a random decade where we don\'t know how many there are, we can estimate it at ' + ((365.25 * 10) * 60 * 24).to_s + ' minutes.'
